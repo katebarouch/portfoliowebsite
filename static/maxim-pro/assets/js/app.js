323e9a1e267c678ac7757731a0c5194aa9660a00
Kate <kate.barouch@gmail.com>
2023-09-12 15:27:24 -0700

    changes to model.py

---
+++ static/maxim-pro/assets/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Path from '.static/maxim-pro/assets/js/path.js';
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <Path />
+        </div>
+    );
+}
+
+export default App;
